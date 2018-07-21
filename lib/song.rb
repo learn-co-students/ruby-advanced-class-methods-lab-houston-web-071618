cffe3d2e9dc3a1fa9ddb4da888bc3e084fca9e27
pabloargueta <pabloargueta27@gmail.com>
2018-07-21 16:28:18 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,68 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+
+
+  def self.create
+    song = self.new
+    all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    all << song
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    all.find { |song|
+      song.name == name
+    }
+  end
+
+  def self.find_or_create_by_name(name)
+    song = find_by_name(name)
+    if song == nil
+      create_by_name(name)
+    else
+      song
+    end
+  end
+
+  def self.alphabetical
+    all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file_name)
+    file_name = file_name.split(/[-.]/)
+    file_name = file_name.map {|word|
+        word.lstrip.rstrip
+    }
+    name = file_name[1]
+    artist = file_name[0]
+
+    find_or_create_by_name(name)
+    find_by_name(name).artist_name = artist
+    find_by_name(name)
+  end
+
+  def self.create_from_filename(file_name)
+    new_from_filename(file_name)
+  end
+
+  def self.destroy_all
+    all.clear
   end
 
 end
